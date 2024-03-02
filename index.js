a3d2f0a0640a031b71d04cc2f040907048574fb5
Skitt <sapolu08@gmail.com>
2024-03-03 00:16:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const MAX_FILE_SIZE = 21 * 1024 * 1024; 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server, {
-  maxHttpBufferSize: 1e8, 
-});
+
+const input = document.getElementById('enviarArchivo')
+
+    input.addEventListener('change', (event) => {
+      const target = event.target
+      if (target.files && target.files[0]) {
+        const maxAllowedSize = 25 * 1024 * 1024;
+        if (target.files[0].size > maxAllowedSize) {
+          target.value = ''
+        }
+      }
+    })
 
 
-app.use(express.static(__dirname + '/public'));
 
+    const socket = io({
+      transports: ['websocket'],
+      upgrade: false,
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: Infinity,
+      autoConnect: true,
+      maxHttpBufferSize: 1e8, 
+    });
 
-app.get('/', function(req,res){
-  res.sendFile(__dirname + "/public/index.html");
-})
+    const id = document.getElementById("tuid");
+   
+    socket.on("connect", () => {
+      id.innerText = socket.id;
 
+      qrcode.clear();
+      qrcode.makeCode("http://vps-44df9322.vps.ovh.net/?id?=" + socket.id);
 
-io.on("connection", (socket) => {
-  socket.on("archivo", (arg) => {
-    if (arg && arg.a.length < MAX_FILE_SIZE) {
-      enviar(arg.id, arg.n, arg.a);
-    } else {
-      console.error("Archivo demasiado grande o inválido");
-    }
-  });
-});
-function enviar (id,n,a){
-  io.emit(id,{
-    "nombre":n,
-    "archivo":a
-  })
-}
+      socket.on(socket.id, (mensaje) => {
+        console.log(mensaje);
 
-server.listen(80, () => {
-  console.log('Servidor escuchando en http://localhost:80');
-});
+        const downloadButton = document.createElement("button");
+        downloadButton.innerText = "Descargar Archivo";
+        downloadButton.addEventListener("click", () => {
+          const linkSource = mensaje.archivo;
+          const downloadLink = document.createElement("a");
+          downloadLink.href = linkSource;
+          downloadLink.download = mensaje.nombre;
+          downloadLink.click();
+        });
+
+        const recibidoArchivoDiv = document.getElementById("recibido_Archivo");
+        recibidoArchivoDiv.innerHTML = "";
+        recibidoArchivoDiv.appendChild(downloadButton);
+      });
+    });
+
