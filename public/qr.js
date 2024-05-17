2077c10cd404a9f44d7a66a61ff1075b370b05b5
Skitt <samuelponceluna@gmail.com>
2024-05-17 17:58:25 +0200

    Delete public/qr.js

--- public/qr.js
+++
@@ -1,8 +0,0 @@
-var qrcode = new QRCode("qrcode", {
-    text: window.location + "/?id=" + ID,
-    width: 128,
-    height: 128,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
-  })
