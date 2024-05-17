f1fd6d93e9167b3fda2a7bcd85658055202bbc34
Skitt <samuelponceluna@gmail.com>
2024-05-17 17:58:17 +0200

    Delete public/params.js

--- public/params.js
+++
@@ -1,8 +0,0 @@
-try {
-  document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("id").value = location.search.split("?id=").pop();
-});
-
-  } catch (error) {
-    console.log("ignora este error.")
-  }
