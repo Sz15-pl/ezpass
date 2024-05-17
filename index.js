83f25b1da29d5f4012a4b85381f290d7157544a9
Skitt <samuelponceluna@gmail.com>
2024-05-17 17:57:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var sqlite3 = require('sqlite3').verbose();
 var express = require('express');
 var http = require('http');
-const jwt = require('jsonwebtoken');
-const socketio = require('socket.io');
-
 const rateLimit = require('express-rate-limit')
-const pug = require('pug')
+const { RateLimiterMemory } = require('rate-limiter-flexible');
 
+
+const Socketlimiter = new RateLimiterMemory({
+  points: 5, 
+  duration: 60, 
+});
 const authenticatedRuta = require('./routes/authenticated')
 const loginRuta = require('./routes/login')
 const registerRuta = require('./routes/register')
@@ -21,32 +22,29 @@ const io = require('socket.io')(server, {
 });
 
 const MAX_FILE_SIZE =  30 * 1024 * 1024; 
-var db = new sqlite3.Database('./database/usuarios.db');
 require('dotenv').config()
 
 
 const limiter = rateLimit({
-	windowMs: 0.1 * 60 * 1000, 
-	limit: 20,
-	standardHeaders: 'draft-7', 
-	legacyHeaders: false, 
-	
+  windowMs: 60 * 1000, 
+  max: 5, 
+  message: "Has excedido el límite de peticiones. Por favor, inténtalo de nuevo más tarde."
+});
 
-
-})
 
 
 
 
 app.set('view engine', 'pug');
-app.use(limiter);
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/public', express.static('public'));
 app.use('/views', express.static('views'));
 
 
-
+app.use('/login', limiter);
+app.use('/register', limiter);
 app.use('/', loginRuta );
 app.use('/', registerRuta);
 app.use('/', authenticatedRuta);
@@ -58,16 +56,28 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + "/public/index.html")
 })
 
+app.get('*', function(req,res){
+  res.sendFile(__dirname + "/public/index.html")
+})
 
 
 
 io.on('connection', (socket) => {
-  socket.on('archivo', handleArchivo(socket));
+
+  const handleConnection = () => {
+  
+    socket.on('archivo', handleArchivo(socket));
+  };
+
+  handleConnection(); 
 });
 
 const handleArchivo = (socket) => {
   return async (data) => {
     try {
+      
+      await Socketlimiter.consume(socket.handshake.address);
+      
       if (!data || !data.a || data.a.size >= MAX_FILE_SIZE || !data.token) {
         throw new Error('Archivo demasiado grande o inválido');
       }
@@ -75,8 +85,8 @@ const handleArchivo = (socket) => {
       const { id, n, a } = data;
       enviar(socket, id, n, a);
     } catch (error) {
-      console.error(error.message);
-      socket.emit('error', { message: error.message });
+      console.error(error);
+      
     }
   };
 };
